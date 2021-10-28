29208ae84b7a611e7479de46c35a5fe4ce98646b
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-10-28 16:49:02 +0300

    edited ReadMe. Added helper to articles.

---
+++ app/helpers/article_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# pokemon
+module ArticleHelper
+  def format_time(time)
+    time.strftime("%b %d %Y, %l:%M%P")
+  end
+
+  def fit_text(text)
+    text.length > 500 ? text[0...500] + '....' : text
+  end
+end
