432520bad78cf3c482190cbb7ff0eb57c5abf24c
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-10-28 18:18:27 +0300

    added MainController test

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Fabricator(:user) do 
+Fabricator(:user) do
   name     { Faker::Name.name }
   email    { Faker::Internet.email }
   password { Faker::Internet.password(min_length: 6) }
