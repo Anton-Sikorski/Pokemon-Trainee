3fd2df0956b2bd3be53fd98bc449ca0dfca6c85f
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-10-28 18:43:21 +0300

    basic rspec test for ArticlesController

--- spec/controllers/article_spec.rb
+++ spec/controllers/article_spec.rb
@@ -4,7 +4,8 @@ require "rails_helper"
 require_relative "../support/devise"
 
 RSpec.describe ArticlesController, type: :controller do
-  article2 = Fabricate(:article)
+  user = Fabricate(:user, created_at: 5.days.ago)
+  article = Fabricate(:article, user_id: user.id)
   describe "GET index" do
     it "returns a successful response for index" do
       get :index
@@ -24,7 +25,7 @@ RSpec.describe ArticlesController, type: :controller do
 
   describe "GET #show" do
     it "returns a successful response for new" do
-      params = { id: article2.id }
+      params = { id: article.id }
       get :show, params: params
       expect(response).to have_http_status(:success)
     end
