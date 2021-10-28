6b104b8a14fb863e84c320f155447262c05ac3d0
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-10-28 18:18:04 +0300

    added MainController test

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,6 +11,5 @@ class MainController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user = current_user
   end
 end
---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Fabricator(:user) do 
+  name     { Faker::Name.name }
+  email    { Faker::Internet.email }
+  password { Faker::Internet.password(min_length: 6) }
+end
