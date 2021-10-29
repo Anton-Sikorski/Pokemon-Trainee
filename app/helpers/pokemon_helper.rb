7cae4a6a877ee18da5c24feda9ecb0fe54968436
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-10-29 11:31:06 +0300

    styles edited

--- app/helpers/pokemon_helper.rb
+++ app/helpers/pokemon_helper.rb
@@ -21,6 +21,8 @@ module PokemonHelper
   def hinted_text_field_tag(name, value = nil, hint = "Click and enter text", options = {})
     value = value.nil? ? hint : value
     text_field_tag name, value,
-                   { onclick: "if($(this).value == '#{hint}'){$(this).value = ''}", onblur: "if($(this).value == ''){$(this).value = '#{hint}'}" }.update(options.stringify_keys)
+                   { onclick: "if($(this).value == '#{hint}'){$(this).value = ''}",
+                     onblur: "if($(this).value == ''){$(this).value = '#{hint}'}" }
+                     .update(options.stringify_keys)
   end
 end
