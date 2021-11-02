c8dea2f593639331d4e8457bd7e7728d089d039f
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-11-02 20:12:58 +0300

    pokemon model cleanup

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,23 +6,4 @@ class Pokemon < ApplicationRecord
   has_many :poke_sprites, dependent: :destroy
 
   validates :name, :pokedex_id, presence: true
-
-  def self.search(search)
-    if search != "" && !search.nil?
-      search.gsub!(/[,.!#&?]/, "")
-      if num?(search)
-        where(pokedex_id: search.gsub(/0+/, ""))
-      else
-        where(["name LIKE ? OR p_type LIKE ?", "%#{search}%", "%#{search}%"]).references(:poke_types)
-      end
-    else
-      all
-    end
-  end
-
-  def self.num?(str)
-    !!Integer(str)
-  rescue ArgumentError, TypeError
-    false
-  end
 end
