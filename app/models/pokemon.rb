5327bd6aedbb9c69d6c92f9844d3cc6204b8b471
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2021-11-02 20:12:58 +0300

    pokemon model cleanup
    ci population fix

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -5,24 +5,5 @@ class Pokemon < ApplicationRecord
   has_many :poke_types
   has_many :poke_sprites
 
-  validates_presence_of :name, :pokedex_id
-
-  def self.search(search)
-    if search != "" && !search.nil?
-      search.gsub!(/[,.!#&?]/, "")
-      if num?(search)
-        where(["pokedex_id = ?", search.gsub(/0+/, "")])
-      else
-        where(["name LIKE ? OR p_type LIKE ?", "%#{search}%", "%#{search}%"]).references(:poke_types)
-      end
-    else
-      all
-    end
-  end
-
-  def self.num?(str)
-    !!Integer(str)
-  rescue ArgumentError, TypeError
-    false
-  end
+  validates :name, :pokedex_id, presence: true
 end
